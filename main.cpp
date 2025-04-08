fa966f77aafc40846e7a2e10391ec201590901f1
AleksandreLatsabidze <121313546+AleksandreLatsabidze@users.noreply.github.com>
2025-04-08 07:27:59 +0000

    Refactor code

--- main.cpp
+++ main.cpp
@@ -1,45 +1,7 @@
 #include <iostream>
 #include <string>
-using namespace std;
-enum LoggingLevel
-{
-    DEBUG,
-    INFO,
-    ERROR,
-};
-class ConsoleLogger
-{
-public:
-    void print(LoggingLevel level, string message) const
-    {
-        if (message.empty())
-        {
-            throw invalid_argument("ცარიელი არ შეიძლება იყოს");
-        }
-        if (message == "null")
-        {
-            throw invalid_argument("არ შეიძლება იყოს 'null'");
-        }
-
-        string levelStr;
-        switch (level)
-        {
-        case DEBUG:
-            levelStr = "DEBUG";
-            break;
-        case INFO:
-            levelStr = "INFO";
-            break;
-        case ERROR:
-            levelStr = "ERROR";
-            break;
-        default:
-            throw invalid_argument("Invalid logging level.");
-        }
-
-        cout << "[" << levelStr << "]: " << message << endl;
-    }
-};
+#include "ConsoleLogger.h"
+#include "LoggingLevel.h"
 
 int main()
 {
@@ -51,9 +13,8 @@ int main()
         logger.print(ERROR, "Application error");
         logger.print(DEBUG, ""); 
     }
-    catch (const exception &e)
+    catch (const std::exception& e)
     {
-        cerr << "Error: " << e.what() << endl;
+        std::cout << "Error: " << e.what() << std::endl;
     }
-    return 0;
 }
