ab45d2c6bce6152fc6d4ec429b6b77c1908dab6c
AleksandreLatsabidze <aleksandrelatsabidze5@gmail.com>
2025-04-09 21:49:18 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main()
         logger.print(DEBUG, "Application started");
         logger.print(INFO, "Application finished");
         logger.print(ERROR, "Application error");
+        logger.print(WARNING, "Appilaciotn Warning");
         logger.print(DEBUG, ""); 
     }
     catch (const std::exception& e)
